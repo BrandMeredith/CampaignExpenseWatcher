c1761b3c4d579d9bf269242e3054d201cb4506f5
Brandon Meredith <brand.meredith@gmail.com>
2019-09-18 17:23:25 -0400

    added clean_data def

--- flaskexample/bernie_model.py
+++ flaskexample/bernie_model.py
@@ -5,3 +5,7 @@ def read_data():
     cmte_header = pd.read_csv("~/Documents/Insight/Data - Insight/cm20/cm_header_file.csv") # pull in header for committee names file
     cmte_names = pd.read_csv("~/Documents/Insight/Data - Insight/cm20/cm.txt", delimiter="|", names=cmte_header.columns) # pull in committee names file
     return df, cmte_names
+
+def clean_data(df, cmte_names):
+    # Add committee names to the dataframe
+    return ( df.set_index('CMTE_ID') ).join(cmte_names.set_index('CMTE_ID'))

dd7f67541ca59c1c847b284285159dd1e513ef6e
Brandon Meredith <brand.meredith@gmail.com>
2019-09-18 17:12:26 -0400

    bug fix: removed index

--- flaskexample/bernie_model.py
+++ flaskexample/bernie_model.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 def read_data():
-    df = pd.read_csv("~/Documents/Insight/Data - Insight/oppexp20/oppexp.csv", low_memory=False, index_col='CMTE_ID' )
+    df = pd.read_csv("~/Documents/Insight/Data - Insight/oppexp20/oppexp.csv", low_memory=False)
     cmte_header = pd.read_csv("~/Documents/Insight/Data - Insight/cm20/cm_header_file.csv") # pull in header for committee names file
     cmte_names = pd.read_csv("~/Documents/Insight/Data - Insight/cm20/cm.txt", delimiter="|", names=cmte_header.columns) # pull in committee names file
     return df, cmte_names
-
-
